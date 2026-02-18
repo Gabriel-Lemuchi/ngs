fb7e0f019e727ad867b0ec4a608030af15e2e001
Gabriel Lemuchi <lemuchigabriel@gmail.com>
2026-02-18 13:05:43 -0300

    movendo logo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,8 +6,8 @@ const Header = ({ toggleMenu, toggleCart}) => {
     <>
     <header>
       <button id="hamburguer_button" onClick={ toggleMenu }><img src="/icons/hamburguer.png" width="40px"/></button>
-      <h1>NEW GEN STORE</h1>
       <img src="/icons/newgenicon.png" width="140px"/>
+      <h1>NEW GEN STORE</h1>
       <button id="carrinho_button" onClick={ toggleCart }><img src="/icons/carrinho-de-compras.png" width="40px"/></button>
     </header>
     </>
